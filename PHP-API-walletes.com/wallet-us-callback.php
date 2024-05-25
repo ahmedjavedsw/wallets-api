e24a5b16e4fd37f1f643e2b2cec568ffd87f9307
ahmedjavedsw <97980839+ahmedjavedsw@users.noreply.github.com>
2024-05-25 21:13:01 +0500

    Update wallet-us-callback.php

--- PHP-API-walletes.com/wallet-us-callback.php
+++ PHP-API-walletes.com/wallet-us-callback.php
@@ -10,7 +10,9 @@
         $apiKey         =   isset($_POST['apiKey']) ? $_POST['apiKey'] : '';
         $userEmail      =   isset($_POST['userEmail']) ? $_POST['userEmail'] : '';
         $amount         =   isset($_POST['amount']) ? $_POST['amount'] : '';
+        $secretPass     =   isset($_POST['secretPass']) ? $_POST['secretPass'] : '';
 
+        
         // Check key is matched with the key given to you.
         // $apiKey is key recieved from wallet us.
         // $key is key provided to you. and these both should be same.
@@ -18,14 +20,19 @@
         // Api Settings
         $email   = 'test@gmail.com';            // test@gmail.com (Email from wallet us)
         $key     = 'H3S7-X8R1-M7O6-Q5V0-R8P8';  //H3S7-X8R1-M7O6-Q5V0-R8P8 (API Key from wallet us)
+        $secPass = 'fAtTdNRnqZhmHgB8I1j24UJcs';  // 25 digits secret call back password from wallet us
 
-        if(($apiKey == $key) && ($userEmail == $email)){
+        // only will work if these 3 conditions matched
+        if(($apiKey == $key) && ($userEmail == $email)  && ($secPass == $secretPass)){
+            // Ok good to go.
+
+            // Check Invoice# exist in your system and its in unpaid state + must check the orderToken code retrive from wallet us. its same code which you sent while creating a order request. sent token by you while placing the order + token retrieve from wallet us ofr invoice should be the same.
             // Ok good to go.
 
             // Check Invoice #exist in your system and its in unpaid state.
 
             // This is example query to check the invoice in system.
-            $row = "SELECT Id, OrderPrice, OrderToken FROM table_name WHERE PaymentStatus = 1 AND Id = '$invId'";
+            $row = "SELECT Id, OrderPrice, OrderToken FROM table_name WHERE PaymentStatus = 1 AND Code = '$unlockCode' AND Id = '$invId'";
             
             // Invoice Exist
             if(isset($row->Id)){
@@ -39,6 +46,10 @@
                     // update invoice
                     $objDBCD14->execute("UPDATE table_name SET PaymentStatus = 2, TransactionId = '$transactionId' WHERE Id = '$invId'");
 
+                    // Trigger Email
+
+                    // Update Credits
+                    
                 }
             }
         }
